f7bb0f5e059e9011e02878ebb458999534ae3c3f
gilangrilhami <gilangrilhami@hotmail.co.uk>
2025-02-26 13:13:28 +0000

    feat: POST endpoint for OpenTelemetry trace data
    This commit adds a POST endpoint to the application
    that accepts OpenTelemetry trace data. The endpoint
    is defined in the `Routing.kt` file and is implemented
    in the `Application.kt` file. The endpoint accepts
    a JProtobuf-encoded data structure that represents
    a list of OpenTelemetry spans. The data is decoded
    and processed by the application.
    
    Endpoint is `POST /v1/traces`.
    
    Signed-off-by: gilangrilhami <gilangrilhami@hotmail.co.uk>

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,8 +1,26 @@
 package com.wolkk
 
 import io.ktor.server.application.*
+import io.ktor.server.request.receiveChannel
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.utils.io.readRemaining
+import kotlinx.io.readByteArray
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest
+import com.google.protobuf.util.JsonFormat
 
 fun Application.configureRouting() {
+    routing {
+        get("/") { call.respondText("Hello, world!") }
+        post("/v1/traces") {
+            val rawBytes: ByteArray = call.receiveChannel().readRemaining().readByteArray()
+            val exportRequest = ExportTraceServiceRequest.parseFrom(rawBytes)
+            // print type of exportRequest
+            println("Type of exportRequest: ${exportRequest::class.java}")
+
+            val json = JsonFormat.printer().print(exportRequest)
+            println("Received trace: $json")
+            call.respondText("Tracing...")
+        }
+    }
 }
