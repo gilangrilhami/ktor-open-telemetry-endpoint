8a84c501d6ce2b0257178378fc71e8294769de44
gilangrilhami <gilangrilhami@hotmail.co.uk>
2025-02-26 13:10:32 +0000

    feat: Application.kt install content negotiation
    To enable content negotiation, we need to install
    the ContentNegotiation feature. This feature is
    used to automatically serialize and deserialize
    the request and response bodies.
    The ContentNegotiation feature is installed by
    calling the install(ContentNegotiation) method on
    the application object.
    
    We include json and protobuf as the supported
    formats for serialization and deserialization.
    
    Signed-off-by: gilangrilhami <gilangrilhami@hotmail.co.uk>

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,12 +3,26 @@ package com.wolkk
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.serialization.kotlinx.protobuf.protobuf
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.protobuf.ProtoBuf
 
 fun main() {
     embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
+@OptIn(kotlinx.serialization.ExperimentalSerializationApi::class)
 fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
+        protobuf(ProtoBuf)
+    }
     configureRouting()
 }
